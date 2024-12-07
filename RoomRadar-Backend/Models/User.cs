90a517d59a13a139a011ef30beb79a396bdb8094
elib00 <joshuanapinas19@gmail.com>
2024-12-07 20:46:18 +0800

    [Joshua] init models folder

---
+++ RoomRadar-Backend/Models/User.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RoomRadar_Backend.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Lacking credentials. First name must be provided.")]
+        public required string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Lacking credentials. Last name must be provided.")]
+        public required string LastName { get; set; }
+
+        [Required(ErrorMessage = "Lacking credentials. Email must be provided.")]
+        public required string Email { get; set; }
+
+        [Required(ErrorMessage = "Lacking credentials. Contact number must be provided.")]
+        public required string ContactNumber { get; set; }
+        
+        public bool IsLandLord { get; set; }
+    }
+}
