b3199f90bc5d8871febb65ce2ef0e562673032a7
elib00 <joshuanapinas19@gmail.com>
2024-12-07 22:16:46 +0800

    [Joshua] Changed user models

--- RoomRadar-Backend/Controllers/UsersController.cs
+++ RoomRadar-Backend/Controllers/UsersController.cs
@@ -1,6 +1,25 @@
-﻿namespace RoomRadar_Backend.Controllers
+﻿using Microsoft.AspNetCore.Mvc;
+using RoomRadar_Backend.Models;
+
+namespace RoomRadar_Backend.Controllers
 {
+    [ApiController]
     public class UsersController: ControllerBase
     {
+        [HttpGet]
+        [Route("api/users")]
+        public JsonResult GetUsers()
+        {
+            return new JsonResult(new User
+            {
+                Id = 1,
+                FirstName = "Joshua",
+                LastName = "Napinas",
+                Email = "joshuanapinas@gmail.com",
+                ContactNumber = "09292260864",
+                IsLandLord = true
+            });
+
+        }    
     }
 }
